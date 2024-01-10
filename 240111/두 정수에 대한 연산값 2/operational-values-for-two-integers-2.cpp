2c8ab317a50691b7020c0f5d3fe0a871a9593ee9
ksan09 <60615541+ksan09@users.noreply.github.com>
2024-01-11 08:30:39 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void Calc(int& a, int& b)
+{
+    if(a < b)
+    {
+        a += 10;
+        b *= 2;
+    }
+    else
+    {
+        a *= 2;
+        b += 10;
+    }
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    Calc(a, b);
+    cout << a << ' ' << b;
+    return 0;
+}

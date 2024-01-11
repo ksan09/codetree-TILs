71b40271b9b2680f68ccfb59ad2552d3739e6c37
ksan09 <60615541+ksan09@users.noreply.github.com>
2024-01-11 10:00:33 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/중앙값 계산 2/get-median-2.cpp
+++ 240111/중앙값 계산 2/get-median-2.cpp
@@ -8,17 +8,17 @@ int arr[101];
 int main() {
     int n;
     cin >> n;
-    for(int i = 0; i < n; ++i)
+    for(int i = 1; i <= n; ++i)
     {
         cin >> arr[i];
     }
 
     sort(arr, arr + n);
 
-    for(int i = 0; i < n; ++i)
+    for(int i = 1; i <= n; ++i)
     {
         if(i % 2 == 1)
-            cout << arr[i / 2] << ' ';
+            cout << arr[i / 2 + 1] << ' ';
     }
 
     return 0;

4bbc5c9b1914b997d481901a9985c72568f2cf78
ksan09 <60615541+ksan09@users.noreply.github.com>
2024-01-16 08:27:08 +0900

    Title: 낮은 지점들, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    unordered_map<int, int> m;
+    int n;
+    cin >> n;
+
+    int key, value;
+    for (int i = 0; i < 5; ++i)
+    {
+        cin >> key >> value;
+        if (m.find(key) != m.end())
+        {
+            if (value < m[key])
+            {
+                m[key] = value;
+            }
+        }
+        else
+        {
+            m.insert({ key, value });
+        }
+    }
+
+    int ans = 0;
+    for (auto pair : m)
+    {
+        ans += pair.second;
+    }
+
+    cout << ans;
+
+    return 0;
+}
